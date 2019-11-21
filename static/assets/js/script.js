baa8cdc495e86575d24232364196e46935140b50
Laura Campa <lauras_here@pm.me>
2019-11-20 17:42:43 -0800

    Resize thumbnail images

--- static/assets/js/script.js
+++ static/assets/js/script.js
@@ -1,21 +1,12 @@
 const menuToggle = document.querySelector('#menu-toggler');
 const html = document.querySelector('html');
 const menuContent = document.querySelector('.nav-content');
-const headerHeight = document.querySelector('header').scrollHeight;
 const navBar = document.querySelector('nav');
 const menuToggleIcon = menuToggle.children;
 
 document.addEventListener('DOMContentLoaded', () => {
     html.classList.toggle('js');
 });
-
-window.onscroll = function() {
-    if ((document.body.scrollTop > headerHeight / 3 || document.documentElement.scrollTop > headerHeight / 3) && (!menuContent.classList.contains('show'))) {
-        navBar.classList.add('shrink');
-    } else if (navBar.classList.contains('shrink')) {
-        navBar.classList.remove('shrink');
-    }
-}
 
 menuToggle.addEventListener('click', () => {
     let expanded = menuToggle.getAttribute('aria-expanded') === 'true' || false;
@@ -24,7 +15,6 @@ menuToggle.addEventListener('click', () => {
    if (menuContent.classList.contains('show')) {
        menuToggleIcon.item(0).innerHTML = 'Close';
        menuToggleIcon.item(1).classList.replace('fa-bars', 'fa-window-close');
-       navBar.classList.remove('shrink');
    } else {
        menuToggleIcon.item(0).innerHTML = 'Menu';
        menuToggleIcon.item(1).classList.replace('fa-window-close', 'fa-bars');
